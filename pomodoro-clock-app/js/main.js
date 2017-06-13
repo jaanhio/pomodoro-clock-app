4f231ff0783b8786b9654770f756edf86ed5ce55
Jianhao Tan <jianhaot.89@gmail.com>
2017-06-14 00:04:30 +0800

    further fine tuning of play/pause buttons

--- pomodoro-clock-app/js/main.js
+++ pomodoro-clock-app/js/main.js
@@ -3,6 +3,8 @@ var breakLength = 5;
 var sessionLength = 25;
 
 $(document).ready(function(){
+
+
   $('#minus-break').on('click',function(){
     if(breakLength>0){
     breakLength--;
@@ -20,11 +22,13 @@ $(document).ready(function(){
     sessionLength--;
   }
     $('#session-time').html(sessionLength);
+    $('#timer').html(sessionLength);
   });
 
   $('#add-session').on('click',function(){
     sessionLength++;
     $('#session-time').html(sessionLength);
+    $('#timer').html(sessionLength);
   });
 
   $('#reset-button').on('click',function(){
@@ -32,5 +36,34 @@ $(document).ready(function(){
     sessionLength = 25;
     $('#break-time').html(breakLength);
     $('#session-time').html(sessionLength);
+    $('#timer').html(sessionLength);
+    $('#status').html("Please select the desired break and session time.");
+    //$('#tips').show();
+    $('#start-pause-button').html("play_arrow");
+  });
+
+  $('#start-pause-button').on('click',function(){
+    if($('#status').text().indexOf('SESSION!')> -1){
+      $('#status').html("BREAK!");
+    }
+    else{
+      $('#status').html("SESSION!");
+    }
+    //$('#tips').hide();
+
+    if($(this).text().indexOf('play_arrow')> -1){
+      $(this).html("pause");
+    }
+    else{
+      $(this).html("play_arrow");
+    }
+    //$('#pause-button').show();
+  });
+
+  $('#pause-button').on('click',function(){
+    $('#status').html("PAUSED!");
+    //$('#tips').hide();
+    $(this).hide();
+    $('#start-button').show();
   });
 });
