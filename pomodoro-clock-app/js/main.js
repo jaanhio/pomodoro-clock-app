cb12e8616f5a8bec84e034a878f226f93e92161f
Jianhao Tan <jianhaot.89@gmail.com>
2017-06-15 08:23:01 +0800

    countdown feature working. need to work on the pause feature

--- pomodoro-clock-app/js/main.js
+++ pomodoro-clock-app/js/main.js
@@ -1,20 +1,24 @@
 var countDownTime = 0;
 var breakLength = 5;
 var sessionLength = 25;
+var intervalSession;
 
 $(document).ready(function(){
-
 
   $('#minus-break').on('click',function(){
     if(breakLength>0){
     breakLength--;
   }
     $('#break-time').html(breakLength);
+    $('#start-pause-button').html("play_arrow");
+    clearInterval(intervalSession);
   });
 
   $('#add-break').on('click',function(){
     breakLength++;
     $('#break-time').html(breakLength);
+    $('#start-pause-button').html("play_arrow");
+    clearInterval(intervalSession);
   });
 
   $('#minus-session').on('click',function(){
@@ -23,12 +27,16 @@ $(document).ready(function(){
   }
     $('#session-time').html(sessionLength);
     $('#timer').html(sessionLength);
+    $('#start-pause-button').html("play_arrow");
+    clearInterval(intervalSession);
   });
 
   $('#add-session').on('click',function(){
     sessionLength++;
     $('#session-time').html(sessionLength);
     $('#timer').html(sessionLength);
+    $('#start-pause-button').html("play_arrow");
+    clearInterval(intervalSession);
   });
 
   $('#reset-button').on('click',function(){
@@ -40,24 +48,100 @@ $(document).ready(function(){
     $('#status').html("Please select the desired break and session time.");
     //$('#tips').show();
     $('#start-pause-button').html("play_arrow");
+    clearInterval(intervalSession);
   });
 
   $('#start-pause-button').on('click',function(){
-    if($('#status').text().indexOf('SESSION!')> -1){
+    /*if($('#status').text().indexOf('SESSION!')> -1){
       $('#status').html("BREAK!");
     }
     else{
       $('#status').html("SESSION!");
-    }
+    }*/
     //$('#tips').hide();
 
     if($(this).text().indexOf('play_arrow')> -1){
       $(this).html("pause");
+
+      var tmpSession = sessionLength;
+      var tmpBreak = breakLength;
+
+      function countDownSession(){
+        //if(sessionLength>0){
+        //sessionLength--;
+        //}
+        //var tmpSession = sessionLength;
+        //var tmpBreak = breakLength;
+        $('#status').html("SESSION!");
+        //$('#timer').html(sessionLength);
+        if(sessionLength >= 0){
+        console.log(sessionLength + " " + $('#status').text());
+        $('#timer').html(sessionLength);
+        sessionLength--;
+
+      }
+        else/*(sessionLength<0)*/{
+          //sessionLength = tmpSession;
+          //breakLength = tmpBreak;
+          countDownBreak();
+          //sessionLength = tmpSession;
+        }
+      }
+
+      function countDownBreak(){
+        $('#status').html("BREAK!");
+        if(breakLength >=0){
+        console.log(breakLength + " " + $('#status').text());
+        $('#timer').html(breakLength);
+        breakLength--;
+        }
+        else/*(breakLength<0)*/{
+          //countDownSession();
+          breakLength = tmpBreak;
+          sessionLength = tmpSession;
+          countDownSession();
+        }
+      }
+        /*if(sessionLength<0){
+          sessionLength = tmpSession;
+          $('#timer').html(breakLength);
+          breakLength--;
+          $('#status').html("BREAK!");
+        }
+
+        if(breakLength<0){
+          breakLength = tmpBreak;
+          $('#timer').html(sessionLength);
+          sessionLength--;
+          $('#status').html("SESSION!");
+        }
+
+      }*/
+
+      intervalSession = setInterval(countDownSession, 1000);
+      //intervalBreak = setInterval(countDownBreak, 1000);
+
+      /*if(sessionLength<0){
+        clearInterval(intervalSessionession);
+        intervalBreak = setInterval(countDownBreak, 1000);
+      }
+
+      if(breakLength<0){
+        clearInterval(intervalBreak);
+        setInterval(countDownSession, 1000);
+      }*/
+
     }
     else{
       $(this).html("play_arrow");
+      clearInterval(intervalSession);
+      $('#status').html("Time Paused!");
     }
-    //$('#pause-button').show();
+
+
+    //console.log(breakLength);
+    //console.log(sessionLength);
+
   });
 
   $('#pause-button').on('click',function(){
